4e2fab78e42731e6cfd0fdc1f80f7a3ea97ed1bb
rssssssssssssssssssssssssssssssssssss <roshinisunjith@gmail.com>
2024-11-13 12:40:05 -0500

    Replaced nov11 with Lab 8

---
+++ Lab8.js
@@ -0,0 +1,85 @@
+//Question 1 and 2
+let name = "Roshini";
+let age = 18;
+console.log(name);
+console.log(age);
+//Question 2
+let Question2 = true;
+let QQuestion2;
+console.log(Question2);
+console.log(QQuestion2);
+//Question 3
+let RollNumber = 15;
+if (RollNumber > 10){
+    console.log("This Number is greater than 10");
+}
+//Question 4
+let isMember = true;
+if (isMember = true){
+    console.log("Member Discount Applied");
+}
+//Question 5
+let StartValue = 50;
+if (StartValue > 0){
+    console.log("Positive value");
+}
+else if (StartValue == 0){
+    console.log("0");
+}
+else{
+    console.log("Negative Value");
+}
+//Question 6
+let A = 10;
+let B = 13;
+console.log(A == B);
+console.log(A === B);
+console.log(A != B);
+console.log(A <= B);
+console.log(A >= B);
+console.log (A < B);
+console.log (A > B);
+//Question 7
+let FirstName = "Roshini";
+let LastName = "Sunjith";
+console.log("Hello " + FirstName + " " + LastName + ", You are " + age + " years old.")
+//Question 8
+let number = 50;
+if (number < 100 && number > 0){
+    console.log("Your nuber is eligible");
+}
+//Question 9
+let HasCar = true;
+let HasLicence = true;
+let HasInsurance = true;
+if (HasCar == false){
+    console.log("Check your eligibilty");
+}
+else if (HasInsurance == false){
+    console.log("Check your eligibilty");
+}
+else if (HasLicence == false){
+    console.log("Check your eligibilty");
+}
+else{
+    console.log("You can drive legally");
+}
+//Question 10
+let num = 21;
+if (number % 2 == 0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+//Question 11
+let score = 95;
+if (score >= 90){
+    console.log ("A");
+}
+else if (score >= 80){
+    console.log("B");
+}
+else{
+    console.log("C");
+}
--- nov11.js
+++
@@ -1,4 +0,0 @@
-let name = "Roshini";
-
-
-console.log(name)
